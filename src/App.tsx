09916bcf172b071bb877d473940d82f3084a6f97
Kachkynov Alikhan <t8t228@gmail.com>
2024-12-20 07:50:30 +0600

    add random number generation and sort

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,34 @@
 import './App.css';
+import Ball from "./components/Ball/Ball.tsx";
+import {useState} from "react";
 
 
 const App = () => {
 
+    const [balls, setBalls] = useState([5, 11, 16, 23, 32]);
+
+    const randomBalls = () => {
+        const randomArr: number[] = [];
+
+        while(randomArr.length < 5) {
+            const randomNumber = Math.floor(Math.random() * (33 - 5 + 1) + 1);
+
+            if (!randomArr.includes(randomNumber)) {
+                randomArr.push(randomNumber);
+            }
+        }
+
+        randomArr.sort((a, b) => a - b);
+
+        setBalls(randomArr);
+    };
+
     return (
         <>
+            <button type='button' onClick={randomBalls}>New numbers</button>
+            <div style={{marginTop: '20px',display: 'flex', justifyContent: 'center', gap: '10px'}}>
+                {balls.map((ball) => <Ball ballNumber={ball}/>)}
+            </div>
         </>
     )
 };
